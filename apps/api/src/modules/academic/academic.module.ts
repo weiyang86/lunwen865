584663b2944638ccd6f64dc53d8727bc2e58945c
jll <jll@admin.com>
2026-06-17 10:02:55 +0800

    本地微调

--- apps/api/src/modules/academic/academic.module.ts
+++ apps/api/src/modules/academic/academic.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from '../../prisma/prisma.module';
 import { AcademicController } from './academic.controller';
 import { AdminAcademicController } from './admin-academic.controller';
 import { AcademicService } from './academic.service';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [AcademicController, AdminAcademicController],
   providers: [AcademicService],
   exports: [AcademicService],
